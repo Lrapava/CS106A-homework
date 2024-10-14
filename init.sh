84b28a011548a24bb050286b1cb455b5ecd0bccd
Luka Rapava <luka.rapava2004@gmail.com>
2024-10-15 02:56:05 +0400

    init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
+# update
 sudo apt update
 sudo apt upgrade -y
-sudo apt install openjdk-17-jdk -y
+
+# installed packages
+sudo apt install openjdk-17-jdk micro -y
+
+# configure java
 sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64
+
+# add custom scripts
 mkdir /home/vscode/.local/bin
 cp /workspaces/CS106A-codespace/scripts/karel /home/vscode/.local/bin
