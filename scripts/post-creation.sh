9224d174d34df4ed2aaad097c74107e5d4a39739
Luka Rapava <luka.rapava2004@gmail.com>
2024-10-15 02:18:43 +0400

    added karel script

--- scripts/post-creation.sh
+++ scripts/post-creation.sh
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-17-jdk -y
+sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64
+mkdir /home/vscode/.local/bin
+cp /workspaces/CS106A-codespace/scripts/karel /home/vscode/.local/bin
