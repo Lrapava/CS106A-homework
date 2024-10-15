88140bb2cc915b5da52a90ac6aacc7c263e1b7ca
Luka Rapava <luka.rapava2004@gmail.com>
2024-10-15 06:42:29 +0400

    verbose README

--- init_codespace.sh
+++ init_codespace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # installing dependencies
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt -y upgrade 
 sudo apt -y install openjdk-17-jdk entr micro
 
 # installing local stuff
